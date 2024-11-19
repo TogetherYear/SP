33e25d9e5d73ff238c1fb01be6e06d352ab0372f
章文俊 <1346502061@qq.com>
2024-11-19 16:35:55 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig(({ command, mode }) => {
+    return {
+        plugins: [],
+        resolve: {
+            alias: {
+                '@': path.resolve('Src')
+            }
+        },
+        esbuild: {
+            drop: command === 'serve' ? [] : ['debugger'],
+            pure: command === 'serve' ? [] : ['console.log']
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler'
+                }
+            }
+        },
+        build: {
+            outDir: path.join(__dirname, 'Dist'),
+            emptyOutDir: true,
+            minify: 'esbuild',
+            assetsDir: 'Source',
+            sourcemap: false,
+            target: 'esnext',
+            rollupOptions: {
+                output: {
+                    manualChunks: (id: string) => {
+                        if (id.includes('node_modules')) {
+                            return 'Vendor';
+                        }
+                    }
+                }
+            }
+        },
+        root: path.join(__dirname, ''),
+        publicDir: 'Public',
+        optimizeDeps: {
+            include: []
+        },
+        base: './',
+        envDir: './Env',
+        server: {
+            host: '0.0.0.0',
+            port: 6768,
+            open: true,
+            strictPort: true
+        }
+    };
+});
